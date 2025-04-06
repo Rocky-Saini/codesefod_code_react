6de500a936728adb76a9f702e87eaa98229fafea
Rocky Saini <rockysaini@MacBooks-MacBook-Pro.local>
2025-04-06 14:48:57 +0530

    normal change

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -170,6 +170,7 @@ const Header = () => {
                             );
                         })}
                     </nav>
+                    
                 </div>
 
                 {/* QR Code */}
